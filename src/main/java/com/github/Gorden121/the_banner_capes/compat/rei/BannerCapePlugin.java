718e5da679bc1e147a89b2a13f43caf80cdfa15d
Gorden121 <mygaminglp@live.de>
2021-11-22 19:15:35 +0100

    REI Finally Solved (With new REI Version)
    No need for a plugin anymore - Yay!

--- src/main/java/com/github/Gorden121/the_banner_capes/compat/rei/BannerCapePlugin.java
+++
@@ -1,9 +0,0 @@
-package com.github.Gorden121.the_banner_capes.compat.rei;
-
-import com.github.Gorden121.the_banner_capes.BannerCapes;
-import com.github.Gorden121.the_banner_capes.compat.rei.common.display.BannerCapeCraftingDisplay;
-import me.shedaniel.rei.api.common.category.CategoryIdentifier;
-
-public interface BannerCapePlugin {
-    CategoryIdentifier<BannerCapeCraftingDisplay<?>> BANNER_CAPE_CRAFTING = CategoryIdentifier.of(BannerCapes.MOD_ID, "plugins/crafting_bannercape_shaped");
-}
